6e5376495a3438662c1424f42adb53e033f2d1a5
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-08-28 21:19:33 +0600

    RXP-1: added a sample example how to ask permissions

--- app/src/main/java/dev/sunnat629/permissionswithrxpermission/MainActivity.kt
+++ app/src/main/java/dev/sunnat629/permissionswithrxpermission/MainActivity.kt
@@ -3,20 +3,34 @@ package dev.sunnat629.permissionswithrxpermission
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tbruyelle.rxpermissions2.RxPermissions
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RXPContract.View {
 
     private lateinit var rxPermissions: RxPermissions
+    private lateinit var presenter: RXPContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         rxPermissions = RxPermissions(this)
-
+        presenter = RXPPresenterImpl(this, rxPermissions)
         askForPermissions()
     }
 
     private fun askForPermissions() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        presenter.askPermissions()
+    }
+
+    override fun onPermissionGranted(permission: String) {
+        Timber.tag("TAG").d("$permission: is granted !")
+    }
+
+    override fun onPermissionDeniedWithoutAskNeverAgain(permission: String) {
+        Timber.tag("TAG").d("$permission: Denied permission without ask never again.")
+    }
+
+    override fun onPermissionDeniedWithAskNeverAgain(permission: String) {
+        Timber.tag("TAG").d("$permission: Denied permission with ask never again. Need to go to the settings.")
     }
 }
