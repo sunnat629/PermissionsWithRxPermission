d465203f8c6bba5403bf8edd04dce4b37e41f4ea
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-08-28 20:41:08 +0600

    RXP-1: initialize rxpermissions in activity

--- app/src/main/java/dev/sunnat629/permissionswithrxpermission/MainActivity.kt
+++ app/src/main/java/dev/sunnat629/permissionswithrxpermission/MainActivity.kt
@@ -2,11 +2,21 @@ package dev.sunnat629.permissionswithrxpermission
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tbruyelle.rxpermissions2.RxPermissions
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rxPermissions: RxPermissions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rxPermissions = RxPermissions(this)
+
+        askForPermissions()
+    }
+
+    private fun askForPermissions() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
